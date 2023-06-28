7046f65caba0d0332f73c6ade99a4ff561cd1f76
Xenepix38 <nico.faglin@gmail.com>
2023-06-28 23:50:06 +0200

    feat(config): work on setup config on dockerfiles & compose command

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -10,6 +10,5 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.server")
-
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.server")
 application = get_asgi_application()
