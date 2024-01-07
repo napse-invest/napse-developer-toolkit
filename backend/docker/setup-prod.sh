1e7a84fb951b076a45868c90350009994aba4e6a
Tom JEANNESSON <tomjeannesson@wificampus-096164.grenet.fr>
2024-01-07 12:37:12 +0100

    fix: tmp

--- backend/docker/setup-prod.sh
+++ backend/docker/setup-prod.sh
@@ -1,4 +1,4 @@
-export GITHUB_AUTH_TOKEN=$(echo "tomjeannesson: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" | tr -d "\n" | base64)
+export GITHUB_AUTH_TOKEN=$(echo "tomjeannesson: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" | tr -d "\n" | base64)
 mkdir -p provisionEB
 touch provisionEB/Dockerrun.aws.json
 touch provisionEB/config.json
