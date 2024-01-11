7a9b5a2508bfead6d0b4b5ba268dd7debf3cfca6
Tom JEANNESSON <tomjeannesson@wificampus-096164.grenet.fr>
2024-01-12 00:14:19 +0100

    fix: tmp

--- backend/deploy/aws/.platform/hooks/prebuild/setup_env_variables.sh
+++ backend/deploy/aws/.platform/hooks/prebuild/setup_env_variables.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 eval $(/opt/elasticbeanstalk/bin/get-config environment | jq -r 'to_entries[] | "\(.key)=\(.value)"')
-/opt/elasticbeanstalk/bin/get-config environment
-export NAPSE_API_DOMAIN=napse-env.eba-vm2tsihk.eu-west-3.elasticbeanstalk.com
